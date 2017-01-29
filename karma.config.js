16341c8a846c3560f21ea44550043110fd6f2385
przemkow <przemkow92@gmail.com>
2017-01-29 16:47:27 +0100

    add test coverage

--- karma.config.js
+++ karma.config.js
@@ -26,7 +26,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.ts': ['typescript', 'browserify']
+      '**/*.ts': ['typescript', 'browserify', 'coverage']
     },
 
     typescriptPreprocessor: {
@@ -44,7 +44,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
@@ -75,6 +75,12 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    }
   })
-}
+};
